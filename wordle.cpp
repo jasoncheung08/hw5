4665696a45bb173966998eac7337ff9fd19589c0
Ka Yiu Jason Cheung <cheungja@usc.edu>
2025-04-25 23:57:01 +0000

    optomize.. final commit

--- wordle.cpp
+++ wordle.cpp
@@ -15,55 +15,9 @@
 using namespace std;
 
 
-// helper
-int countChar(const string& s, char c) {
-  int count = 0;
-  for(char ch : s) {
-    if(ch == c) count++;
-  }
-  return count;
-}
+// helper declaration
+void generateWords(string& current, int index, multiset<char>& floating, const set<string>& dict, set<string>& results, int totalDashes);
 
-void helper(string& current, int idx, string floating, const set<string>& dict, set<string>& results) {
-  if (idx == current.size()) {
-    bool valid = true;
-    for(char c : floating) {
-      if(countChar(current, c) < countChar(floating, c)) {
-        valid = false;
-        break;
-      }
-    }
-    if(valid && dict.find(current) != dict.end()) {
-      results.insert(current);
-    }
-    return;
-  }
-
-  if(current[idx] != '-') {
-    helper(current, idx + 1, floating, dict, results);
-  }
-  else {
-    for(char c = 'a'; c <= 'z'; ++c) {
-      current[idx] = c;
-
-      int pos = -1;
-      for(size_t i = 0; i < floating.size(); ++i) {
-        if(floating[i] == c) {
-          pos = i;
-          break;
-        }
-      }
-
-      string newFloating = floating;
-      if(pos != -1) {
-        newFloating.erase(pos, 1);
-      }
-
-      helper(current, idx + 1, newFloating, dict, results);
-    }
-    current[idx] = '-';
-  }
-}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -73,7 +27,56 @@ std::set<std::string> wordle(
 {
     // Add your code here
     set<string> results;
+    multiset<char> floatingSet;
+    for (char c : floating) {
+      floatingSet.insert(c);
+    }
+
     string current = in;
-    helper(current, 0, floating, dict, results);
+    int dashCount = 0;
+    for (char c : in) {
+      if (c == '-') ++dashCount;
+    }
+
+    generateWords(current, 0, floatingSet, dict, results, dashCount);
     return results;
+}
+
+// recursive helper
+void generateWords(string& current, int index, multiset<char>& floating, const set<string>& dict, set<string>& results, int totalDashes) {
+  if (index == (int)current.size()) {
+    if (floating.empty() && dict.find(current) != dict.end()) {
+      results.insert(current);
+    }
+    return;
+  }
+
+  if (current[index] != '-') {
+    generateWords(current, index + 1, floating, dict, results, totalDashes);
+    return;
+  }
+
+  int remainingDashes = 0;
+  for (int i = index; i < (int)current.size(); ++i) {
+    if (current[i] == '-') remainingDashes++;
+  }
+
+  for (char c = 'a'; c <= 'z'; ++c) {
+    bool isFloating = (floating.find(c) != floating.end());
+
+    if ((int)floating.size() > remainingDashes) return;
+
+    current[index] = c;
+
+    if (isFloating) {
+      floating.erase(floating.find(c));
+      generateWords(current, index + 1, floating, dict, results, totalDashes);
+      floating.insert(c);
+    }
+    else if ((int)floating.size() <= remainingDashes - 1) {
+      generateWords(current, index + 1, floating, dict, results, totalDashes);
+    }
+  }
+
+  current[index] = '-';
 }
